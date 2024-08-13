dea4be61d25087e8f5251815230db85ffcc4c7ab
Igor Castelo <freitascasteloigor@gmail.com>
2024-08-13 16:04:40 -0300

    --

---
+++ src/SistemaGerenciamentoSemanario/domain/Seminario.java
@@ -0,0 +1,50 @@
+package SistemaGerenciamentoSemanario.domain;
+
+public class Seminario {
+    private String titulo;
+    private Local local;
+    private Aluno[] alunos;
+
+
+    public Seminario(String titulo, Local local) {
+        this.titulo = titulo;
+        this.local = local;
+    }
+
+    public Seminario(String titulo, Local local, Aluno[] alunos) {
+        this.titulo = titulo;
+        this.local = local;
+        this.alunos = alunos;
+    }
+
+    public void imprimir() {
+        for (Aluno aluno : alunos) {
+            System.out.println(aluno.getNome());
+        }
+
+    }
+
+    public Aluno[] getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(Aluno[] alunos) {
+        this.alunos = alunos;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Local getLocal() {
+        return local;
+    }
+
+    public void setLocal(Local local) {
+        this.local = local;
+    }
+}
