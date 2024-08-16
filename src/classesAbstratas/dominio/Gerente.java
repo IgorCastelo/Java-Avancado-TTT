5879f118d6239dca73e27150a1da027459e5fe84
Igor Castelo <freitascasteloigor@gmail.com>
2024-08-16 17:59:50 -0300

    Classes abstratas2: Métodos Abstratos

--- src/classesAbstratas/dominio/Gerente.java
+++ src/classesAbstratas/dominio/Gerente.java
@@ -4,4 +4,9 @@ public class Gerente extends Funcionario{
     public Gerente(String nome, double salario) {
         super(nome, salario);
     }
+
+    @Override
+    public void calculaBonus() {
+        this.salario+=this.salario*0.2;
+    }
 }
