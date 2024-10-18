dfc4af5cb2f87a3b48431aca29395c75f67e392f
Igor Castelo <freitascasteloigor@gmail.com>
2024-10-18 18:16:12 -0300

    Collections: Equals part 1 and 2

--- src/Ycolecoes/dominio/Smartphone.java
+++ src/Ycolecoes/dominio/Smartphone.java
@@ -1,35 +1,49 @@
 package Ycolecoes.dominio;
 
 public class Smartphone {
-    private String seialNumber;
-    private String modelo;
+    private String serialNumber;
+    private String marca;
 
-    public Smartphone(String seialNumber, String modelo) {
-        this.seialNumber = seialNumber;
-        this.modelo = modelo;
+    public Smartphone(String serialNumber, String marca) {
+        this.serialNumber = serialNumber;
+        this.marca = marca;
     }
 
-    public String getSeialNumber() {
-        return seialNumber;
+    public String getSerialNumber() {
+        return serialNumber;
     }
 
-    public void setSeialNumber(String seialNumber) {
-        this.seialNumber = seialNumber;
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
     }
 
-    public String getModelo() {
-        return modelo;
+    public String getMarca() {
+        return marca;
     }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    //reflexivo: x.equals(x) deve ser true para tudo que for diferente de null
+    //Simetrico:  para x  e y diferentes de null,  se x.equals(y), logo y.equals(x) is true
+    //transitividade: Para x,y,z diferentes de null, se x.equals(y)  == true , e x.equals(z)==true, então y.equals(z)==true
+    // consistente x.equals(y) sempre retorna true se x for diferente de null
+    // para x diferente de null, x.equals(null) tem que retornar false.
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (obj.getClass() != this.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return this.serialNumber != null && this.serialNumber.equals(smartphone.serialNumber);
     }
 
     @Override
     public String toString() {
         return "Smartphone{" +
-                "seialNumber='" + seialNumber + '\'' +
-                ", modelo='" + modelo + '\'' +
+                "seialNumber='" + serialNumber + '\'' +
+                ", modelo='" + marca + '\'' +
                 '}';
     }
 }
---
+++ src/Ycolecoes/test/EqualsTeste01.java
@@ -0,0 +1,11 @@
+package Ycolecoes.test;
+
+public class EqualsTeste01 {
+    public static void main(String[] args) {
+        /*String nome  = "Igor Castelo";
+        String nome2  = new String("Igor Castelo");
+        System.out.println(nome.equals(nome2));
+        */
+
+    }
+}
